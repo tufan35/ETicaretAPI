02e89bba4a4822964b5b760996404eff8f928f22
tufan35 <34610153+tufan35@users.noreply.github.com>
2022-04-17 00:32:49 +0300

    Tracking Performans Optimizasyonu eklenmiştir.

--- Presentation/ETicaretAPI.API/Controllers/ProductController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -22,13 +22,19 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new() {Id = Guid.NewGuid(), Name = "Product 1 " , Price  = 100, CreatedDate = DateTime.UtcNow.Date,Stock = 10 },
-                new() {Id = Guid.NewGuid(), Name = "Product 2 " , Price  = 200, CreatedDate = DateTime.UtcNow.Date,Stock = 20 },
-                new() {Id = Guid.NewGuid(), Name = "Product 3 " , Price  = 300, CreatedDate = DateTime.UtcNow.Date,Stock = 10 }
-            });
-            var count = await _productWriteRepository.SaveAsync();
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() {Id = Guid.NewGuid(), Name = "Product 1 " , Price  = 100, CreatedDate = DateTime.UtcNow.Date,Stock = 10 },
+            //    new() {Id = Guid.NewGuid(), Name = "Product 2 " , Price  = 200, CreatedDate = DateTime.UtcNow.Date,Stock = 20 },
+            //    new() {Id = Guid.NewGuid(), Name = "Product 3 " , Price  = 300, CreatedDate = DateTime.UtcNow.Date,Stock = 10 }
+            //});
+            //var count = await _productWriteRepository.SaveAsync();
+
+            Product p = await _productReadRepository.GetByIdAsync("424874a7-0f40-4f5b-a403-79e3f29d3117",false);
+            p.Name = "memet";
+            await _productWriteRepository.SaveAsync();
+
+
         }
 
         [HttpGet("{id}")]
